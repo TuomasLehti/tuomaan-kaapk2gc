6b7316e5d7f3fcbb3dde9c43d3e99162810e032a
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-05-29 23:52:30 +0300

    refactor: lisää ajopäiväkirjamuuttajan logiikan

--- src/main/java/fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/IcsAjopäiväkirjaMuuttaja.java
+++ src/main/java/fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/IcsAjopäiväkirjaMuuttaja.java
@@ -1,10 +1,12 @@
 package fi.tuomaan.joukkoliikenne.sovellukset.kaapk2gc.icskirjoittaja;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import biweekly.component.VEvent;
 import fi.tuomaan.joukkoliikenne.koivistonauto.ajopaivakirja.Ajopäiväkirja;
+import fi.tuomaan.joukkoliikenne.koivistonauto.ajopaivakirja.Työpäivä;
 
 public class IcsAjopäiväkirjaMuuttaja 
 {
@@ -13,7 +15,11 @@ public class IcsAjopäiväkirjaMuuttaja
     {
         List<VEvent> listOfEvents = new ArrayList<>();
 
-
+        for (LocalDate pvm : ajopäiväkirja.työpäivät()) {
+            Työpäivä työpäivä = ajopäiväkirja.työpäivä(pvm);
+            if (työpäivä.sisältääAjopaloja())
+                listOfEvents.addAll(IcsTyöpäiväMuuttaja.muuta(työpäivä));
+        }
 
         return listOfEvents;
 
