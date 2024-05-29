c57c9cc7ee1487c06dc19a4db6767dbaf9bc9112
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-05-29 11:50:35 +0300

    refactor: työpäivämuuttaja muuttaa oikean määrän ajopaloja

--- src/main/java/fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/IcsTyöpäiväMuuttaja.java
+++ src/main/java/fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/IcsTyöpäiväMuuttaja.java
@@ -1,9 +1,12 @@
 package fi.tuomaan.joukkoliikenne.sovellukset.kaapk2gc.icskirjoittaja;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import biweekly.component.VEvent;
+import fi.tuomaan.joukkoliikenne.koivistonauto.ajopaivakirja.Ajopala;
+import fi.tuomaan.joukkoliikenne.koivistonauto.ajopaivakirja.AjopalanTyyppi;
 import fi.tuomaan.joukkoliikenne.koivistonauto.ajopaivakirja.Työpäivä;
 
 /**
@@ -15,7 +18,24 @@ public class IcsTyöpäiväMuuttaja
     public static List<VEvent> muuta(Työpäivä työpäivä)
     {
         List<VEvent> listOfEvents = new ArrayList<>();
+
+        for (Ajopala ajopala : työpäivä.ajopalat())
+          if (muutetaanko(ajopala))
+            listOfEvents.add(IcsAjopalaMuuttaja.muuta(ajopala));
+
         return listOfEvents;
+    }
+
+    private static Boolean muutetaanko(Ajopala ajopala)
+    {
+        List<AjopalanTyyppi> muutettavat = new ArrayList<>(Arrays.asList(new AjopalanTyyppi[]{
+            AjopalanTyyppi.LINJA_AJOA,
+            AjopalanTyyppi.SIIRTOAJO,
+            AjopalanTyyppi.SIIRTYMÄ_AUTOLLA,
+            AjopalanTyyppi.SISÄÄNAJO,
+            AjopalanTyyppi.ULOSAJO
+        }));
+        return muutettavat.contains(ajopala.tyyppi());
     }
 
     
