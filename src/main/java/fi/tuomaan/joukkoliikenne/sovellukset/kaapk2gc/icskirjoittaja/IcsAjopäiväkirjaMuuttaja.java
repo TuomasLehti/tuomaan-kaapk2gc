c3903a91646d6423502d3edf305e034d7b7bb22b
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2024-05-29 19:18:37 +0300

    refactor: lisää ensimmäisen testin

--- src/main/java/fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/IcsAjopäiväkirjaMuuttaja.java
+++ src/main/java/fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/IcsAjopäiväkirjaMuuttaja.java
@@ -1,6 +1,22 @@
 package fi.tuomaan.joukkoliikenne.sovellukset.kaapk2gc.icskirjoittaja;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import biweekly.component.VEvent;
+import fi.tuomaan.joukkoliikenne.koivistonauto.ajopaivakirja.Ajopäiväkirja;
+
 public class IcsAjopäiväkirjaMuuttaja 
 {
     
+    public static List<VEvent> muuta(Ajopäiväkirja ajopäiväkirja)
+    {
+        List<VEvent> listOfEvents = new ArrayList<>();
+
+
+
+        return listOfEvents;
+
+    }
+
 }
--- src/test/java/fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/TestIcsAjopäiväkirjaMuuttaja.java
+++ src/test/java/fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/TestIcsAjopäiväkirjaMuuttaja.java
@@ -1,7 +1,24 @@
 package fi.tuomaan.joukkoliikenne.sovellukset.kaapk2gc.icskirjoittaja;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import biweekly.component.VEvent;
+import fi.tuomaan.joukkoliikenne.koivistonauto.ajopaivakirja.Ajopäiväkirja;
+
 public class TestIcsAjopäiväkirjaMuuttaja 
 extends IcsTestienPohja
 {
+
+    @Test
+    public void lataaOikeanMääränTyöpäiviä()
+    {
+        Ajopäiväkirja ajopäiväkirja = lataaAjopäiväkirja("fi/tuomaan/joukkoliikenne/sovellukset/kaapk2gc/icskirjoittaja/2024-05-20.pdf");
+        List<VEvent> eventList = IcsAjopäiväkirjaMuuttaja.muuta(ajopäiväkirja);
+        assertEquals(5, eventList.size());
+    }
     
 }
